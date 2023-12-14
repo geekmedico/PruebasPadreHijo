5b7408dce505481dfa846296e11ae8222d85b637
Mario Manuel Greer Sequera <mgr33r@live.com.mx>
2023-12-13 22:46:11 -0600

    Add project files.

---
+++ PruebasPadreHijo/HijaPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PruebasPadreHijo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HijaPage : ContentPage
+    {
+        Action<string> _callBack;
+        public string Texto { get; set; } = string.Empty;
+        public HijaPage()
+        {
+            InitializeComponent();
+        }
+        //Creamos esta Constructor para poder inicia nuetra página con el parametro que deseamos
+        public HijaPage(Action<string> callBack)
+        {
+            InitializeComponent();
+            _callBack = callBack;
+            
+        }
+
+        private async void btnCerrar_Clicked(object sender, EventArgs e)
+        {
+            //Al seleccionar el boton cerrar, ejecutamos el metodo CallBack
+            _callBack(txtTextoHijo.Text);
+            await Navigation.PopAsync();
+        }
+        //Esta es otra forma de hacerlo
+        //Así nos aseguramos de que en caso de que el usuario presione el botón físico de retroceso en android, se dispare CallBack
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            //En este caso, necesitamos que el texto sea una propiedad de la HijaPage, para poder usarlo en la MainPage
+            Texto = txtTextoHijo.Text;
+            //O bien, podemos ejecutar CallBack
+            //_callBack(txtTextoHijo.Text);
+        }
+    }
+}
---
+++ PruebasPadreHijo/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PruebasPadreHijo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirHija_Clicked(object sender, EventArgs e)
+        {
+            //Esta es una forma, primer creamos una instancia de HijaPage y no necesitamos pasar CallBack como un parametro
+            //Así nos suscribimos al evento Disappearing de HijaPage
+            //este evento se llamará cuando HijaPage se cierre
+            HijaPage nuevaPagina = new HijaPage();
+            nuevaPagina.Disappearing += NuevaPagina_Disappearing;
+            await Navigation.PushAsync(nuevaPagina);
+
+
+            //Otra forma sería crear la instancia cuando llamamos a presentarla y pasar CallBack como parametro
+            //await Navigation.PushAsync(new HijaPage(CallBack));
+
+            //Otra forma puede ser crear la instancia HijaPage y pasar CallBack como un parametro
+            //Sin suscribirnos al evento Disappearing
+            //HijaPage nuevaPagina = new HijaPage(CallBack);
+            //await Navigation.PushAsync(nuevaPagina);
+        }
+
+        private void NuevaPagina_Disappearing(object sender, EventArgs e)
+        {
+            if (sender is HijaPage hijaPage)
+            {
+                CallBack(hijaPage.Texto);
+                hijaPage.Disappearing -= NuevaPagina_Disappearing;
+                //Nos desuscribimos del evento
+            }
+            
+        }
+
+        //Para ejemplificar uso un parametro string, pero puede ser un modelo que tu determines
+        public void CallBack(string texto)
+        {
+            txtTexto.Text = texto;
+        }
+    }
+}
